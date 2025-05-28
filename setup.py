178990df828cbae430fac88a275c70b8594616c2
Luis Angel <luisangelnunezpinzonskolmi@gmail.com>
2025-05-28 18:50:03 -0500

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="QuintoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Onls",                                  # Tu nombre
+    author_email="luisangelnunezpinzonskolmi@.com", # Tu correo electrónico
+    url="https://github.com/onls294/QuintoRepo#",   # URL del proyecto
+)
